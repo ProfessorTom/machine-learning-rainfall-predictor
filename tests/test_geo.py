175032fbbc705453013f7034b3318ddc58a41ef0
Tomas Gallucci <professortom@gmail.com>
2026-08-26 15:40:07 -0500

    Add ZIP code geocoding with GeocodedZip type
    - Implement geocode_zip() using Zippopotam.us API
    - Return structured GeocodedZip dataclass
    - Add validation and geocoding unit tests
    - Organize tests into classes

--- tests/test_geo.py
+++ tests/test_geo.py
@@ -1,36 +1,56 @@
 # tests/test_geo.py
 
 from hamcrest import assert_that, is_
-from geo import is_valid_us_zip
+from geo import is_valid_us_zip, geocode_zip
+from geocoded_zip import GeocodedZip
 
 
-def test_zip_not_digits():
-    assert_that(is_valid_us_zip("abcde"), is_(False))
-    assert_that(is_valid_us_zip("123a5"), is_(False))
+class IsValidUsZipTests:
+    @staticmethod
+    def test_zip_not_digits():
+        assert_that(is_valid_us_zip("abcde"), is_(False))
+        assert_that(is_valid_us_zip("123a5"), is_(False))
+
+    @staticmethod
+    def test_zip_has_spaces():
+        assert_that(is_valid_us_zip("90 210"), is_(False))
+
+    @staticmethod
+    def test_zip_is_empty_string():
+        assert_that(is_valid_us_zip(""), is_(False))
+
+    @staticmethod
+    def test_zip_too_short():
+        assert_that(is_valid_us_zip("123"), is_(False))
+
+    @staticmethod
+    def test_zip_too_long():
+        assert_that(is_valid_us_zip("123456"), is_(False))
+
+    @staticmethod
+    def test_zip_plus_4_not_supported():
+        assert_that(is_valid_us_zip("20500+1600"), is_(False))
+        assert_that(is_valid_us_zip("20500-1600"), is_(False))
+
+    @staticmethod
+    def test_zip_just_right():
+        assert_that(is_valid_us_zip("12345"), is_(True))
+        assert_that(is_valid_us_zip("90210"), is_(True))
+        assert_that(is_valid_us_zip("00000"), is_(True))
 
 
-def test_zip_has_spaces():
-    assert_that(is_valid_us_zip("90 210"), is_(False))
+class GecodeZipTests:
+    @staticmethod
+    def test_geocode_zip_invalid_zip():
+        assert_that(geocode_zip("abcde"), is_(None))
 
-
-def test_zip_is_empty_string():
-    assert_that(is_valid_us_zip(""), is_(False))
-
-
-def test_zip_too_short():
-    assert_that(is_valid_us_zip("123"), is_(False))
-
-
-def test_zip_too_long():
-    assert_that(is_valid_us_zip("123456"), is_(False))
-
-
-def test_zip_plus_4_not_supported():
-    assert_that(is_valid_us_zip("20500+1600"), is_(False))
-    assert_that(is_valid_us_zip("20500-1600"), is_(False))
-
-
-def test_zip_just_right():
-    assert_that(is_valid_us_zip("12345"), is_(True))
-    assert_that(is_valid_us_zip("90210"), is_(True))
-    assert_that(is_valid_us_zip("00000"), is_(True))
+    @staticmethod
+    def test_geocode_zip_happy_path():
+        data = GeocodedZip(
+            zip = "90210",
+            latitude = 34.0901,
+            longitude = -118.4065,
+            city = "Beverly Hills",
+            state_abbr = "CA"
+        )
+        assert_that(geocode_zip("90210"), is_(data))
